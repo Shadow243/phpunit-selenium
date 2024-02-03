a54419ed8ac8e09d142813973784088560702401
Steven Ngesera <muhngesteven@gmail.com>
2024-02-04 01:14:08 +0200

    Add PHPUnit 10 compatibility

--- .ci/vagrant_pre_setup.sh
+++ .ci/vagrant_pre_setup.sh
@@ -8,5 +8,5 @@ apt-get install software-properties-common -y
 apt-get install python-software-properties -y
 
 # installing xvfb, java and php
-apt-get install xvfb php7.3-cli php7.3-curl php7.3-xml php7.3-mbstring php-xdebug ncurses-term unzip xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic vim -y --no-install-recommends
+apt-get install xvfb php8.1-cli php8.1-curl php8.1-xml php8.1-mbstring php-xdebug ncurses-term unzip xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic vim -y --no-install-recommends
 
